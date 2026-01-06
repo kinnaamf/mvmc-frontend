406ec83ac4fcf2b8ef7ec0a6ca44d358d17b7631
kinnaamf <dimoje.1337@gmail.com>
2026-01-06 16:23:38 +0200

    feat: edited nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://api.mvmc.work'
+    }
+  },
 })
