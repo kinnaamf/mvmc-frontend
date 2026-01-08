971ddcf899fd2b3c89398b3327bd8f9ac5cea253
kinnaamf <dimoje.1337@gmail.com>
2026-01-08 23:23:46 +0200

    feat: add subscription composable

---
+++ app/composables/useSubscription.ts
@@ -0,0 +1,30 @@
+import type { Subscription } from "~/types/subscription";
+
+export const useSubscription = () => {
+  const api = useApi();
+
+  const subscriptions = useState<Subscription[]>('subscriptions', () => []);
+
+  const loading = ref<boolean>(false);
+
+  const getSubscriptions = async () => {
+    if (subscriptions.value.length > 0) return
+
+    loading.value = true;
+
+    try {
+      subscriptions.value = await api<Subscription[]>(`/subscriptions`);
+    } catch (error) {
+      loading.value = false;
+      console.error(error);
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  return {
+    getSubscriptions,
+    subscriptions,
+    loading,
+  }
+}
