aec87ddc8db30a95fef55b6f29b0eb54249f7d65
kinnaamf <dimoje.1337@gmail.com>
2026-01-09 19:09:27 +0200

    feat: add types in useUserPurchases composable

--- app/composables/useUserPurchases.ts
+++ app/composables/useUserPurchases.ts
@@ -1,6 +1,9 @@
+import type { Item } from "~/types/item";
+import type { Subscription } from "~/types/subscription";
+
 export const useUserPurchases = () => {
   const purchasedItems = useState<Item[]>('purchasedItems', () => []);
-  const purchasedSubscriptions = useState<Item[]>('purchasedItems', () => []);
+  const purchasedSubscriptions = useState<Subscription[]>('purchasedSubscriptions', () => []);
 
   return {
     purchasedItems,
