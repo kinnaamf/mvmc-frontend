bc751a763a5ea11fb130fffdb17d03fd626cdecb
kinnaamf <dimoje.1337@gmail.com>
2026-01-09 19:15:55 +0200

    feat: add empty subs, items text on page

--- app/pages/favorites/items.vue
+++ app/pages/favorites/items.vue
@@ -1,9 +1,13 @@
 <template>
-  {{ $route.path }}
+  <div v-if="purchasedItems.length === 0" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <span class="text-white/80 font-medium text-base">Нет купленных товаров</span>
+  </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   layout: 'tabs'
 })
+
+const { purchasedItems } = useUserPurchases()
 </script>
--- app/pages/favorites/subscriptions.vue
+++ app/pages/favorites/subscriptions.vue
@@ -1,9 +1,14 @@
 <template>
-  {{ $route.path }}
+  <div v-if="purchasedSubscriptions.length === 0" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <span class="text-white/80 font-medium text-base">Нет активных подписок</span>
+  </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   layout: 'tabs'
 })
+
+const { purchasedSubscriptions } = useUserPurchases()
+
 </script>
