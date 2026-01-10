81861cee640aabab936dca4e3e346b5c6f1a9d08
kinnaamf <dimoje.1337@gmail.com>
2026-01-10 17:42:51 +0200

    feat: add profile page (wip)

--- app/pages/profile/index.vue
+++ app/pages/profile/index.vue
@@ -3,7 +3,18 @@
 </script>
 
 <template>
-
+  <div class="mt-12">
+    <div class="flex flex-col items-center justify-center">
+      <div class="flex flex-col gap-3">
+        <div>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpFb9q4YTo6wXrUBm1qz7c3jfc47rclFLEVQ&s" alt="" class="w-24 h-24 rounded-full object-cover" />
+        </div>
+        <div>
+          <span class="font-medium">@username</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="postcss">
