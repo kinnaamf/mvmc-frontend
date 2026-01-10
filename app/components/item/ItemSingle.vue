d8d5faf12cecce188c7a8fcac12a52b0bacae2ff
kinnaamf <dimoje.1337@gmail.com>
2026-01-10 22:05:13 +0200

    feat: add cursor pointer on products cards

--- app/components/item/ItemSingle.vue
+++ app/components/item/ItemSingle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card-container px-5 py-8">
+  <div class="card-container px-5 py-8 cursor-pointer">
     <div class="flex justify-center">
       <img :src="item.avatar_url" alt="" class="w-16 h-16 rounded-2xl">
     </div>
