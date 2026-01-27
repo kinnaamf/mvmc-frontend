5ef273e048372b6f84f4444b10e3cc48c524330c
kinnaamf <dimoje.1337@gmail.com>
2026-01-27 19:49:08 +0200

    feat: some changes

---
+++ app/components/subscription/PurchasedList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="space-y-3 pb-28">
+    <PurchasedSubscription
+        v-for="subscription in subscriptions"
+        :key="subscription.id"
+        :subscription="subscription"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Subscription } from "~/types/subscription";
+import PurchasedSubscription from "~/components/subscription/PurchasedSubscription.vue";
+
+defineProps<{
+  subscriptions: Subscription[]
+}>()
+</script>
--- app/pages/favorites/subscriptions.vue
+++ app/pages/favorites/subscriptions.vue
@@ -2,13 +2,16 @@
   <div v-if="purchasedSubscriptions.length === 0" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
     <span class="text-white/80 font-medium text-base">Нет активных подписок</span>
   </div>
+  <PurchasedList :subscriptions="subscriptions"/>
 </template>
 
 <script setup lang="ts">
+import PurchasedList from "~/components/subscription/PurchasedList.vue";
+
 definePageMeta({
   layout: 'tabs'
 })
 
 const { purchasedSubscriptions } = useUserPurchases()
-
+const { subscriptions } = useSubscriptions()
 </script>
